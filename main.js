f1bd8cb5f2383909bab97bf2086b63a26c57a3e5
Jonathan Wernersson <jonathan2008@live.se>
2018-08-31 11:39:27 +0200

    Fixed responsiveness bug when screen was made large again

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 var hamburger = document.getElementsByClassName('hamburger')[0];
+var sideBar = document.getElementsByTagName('nav')[0];
+var container = document.getElementsByClassName('container')[0];
 hamburger.addEventListener("click", displaySidebar);
+
+function largeScreen(x){
+	let nav = document.getElementsByTagName('nav')[0];
+	if(x.matches){
+		nav.style.display = "block";
+		nav.style.overflow = "visible";
+		container.style.opacity = 1;
+		document.body.style.backgroundColor = "white";
+	}
+
+	else{
+		nav.style.display = "none";
+		nav.style.overflow = "scroll";
+	}
+}
 
 function showDrop(){
 	let el = document.getElementsByClassName('drop-down')[0];
@@ -9,13 +26,18 @@ function showDrop(){
 }
 
 function displaySidebar(){
-	let sideBar = document.getElementsByTagName('nav')[0];
 	if(sideBar.style.display == "none"){
-		document.getElementsByClassName('container')[0].style.opacity = 0.5;
+		container.style.opacity = 0.5;
+		document.body.style.backgroundColor = "#aaa";
 		sideBar.style.display = "block";
 	}
 	else{
 		document.getElementsByClassName('container')[0].style.opacity = 1;
 		sideBar.style.display = "none";
+		document.body.style.backgroundColor = "white";
 	}
 }
+
+var x = window.matchMedia("(min-width: 1130px)");
+largeScreen(x);
+x.addEventListener("change",largeScreen);
